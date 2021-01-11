8a0ebe872e15cd3dbac1bb4d174ac6878cd75ead
Amittras <pal.amittras@gmail.com>
2021-01-11 17:46:51 +0530

    corrections app.py

--- app/app.py
+++ app/app.py
@@ -23,3 +23,4 @@ def index():
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=6174, debug=True)
     # to run the app ONLY in the default localhost:5000 or 127.0.0.1:5000, use line
+    # app.run(debug=True)
