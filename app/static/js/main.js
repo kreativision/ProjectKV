26d83797d2af43964ede8e5242335c7e1191a72c
Amittras <pal.amittras@gmail.com>
2021-01-12 18:24:00 +0530

    Sidebar look and feel changes

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -5,6 +5,7 @@
 // Sidebar menu controls.
 function openSidebar() {
     document.querySelector('#sidebar').classList.add('active');
+    document.querySelector('#sidebar button').focus();
 }
 
 function closeSidebar() {
