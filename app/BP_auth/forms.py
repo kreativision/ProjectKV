933b35500bc718e81322d08bf4c0f14a23f7cc2d
Amittras <pal.amittras@gmail.com>
2021-01-16 12:29:54 +0530

    updated registration form and added db email check for login form

--- app/BP_auth/forms.py
+++ app/BP_auth/forms.py
@@ -1,19 +1,21 @@
 # Classes for the Flask forms used in the authentication module.
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationError
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField
+from wtforms.validators import DataRequired, Email, EqualTo, Length, NumberRange, Optional, ValidationError
 from markupsafe import Markup
 from flask.helpers import url_for
 from app.models import User
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField("Full Name", validators=[DataRequired(), Length(min=3, max=25)])
-    email = StringField("E-mail", validators=[DataRequired(), Email()])
-    password = PasswordField("Password", validators=[DataRequired()])
-    cnf_password = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo('password')])
+    # render_kw => add autofocus feature.
+    username = StringField("Full Name *", validators=[DataRequired(), Length(min=3, max=50, message="Name must be 3 to 50 characters long")], render_kw={'autofocus': True})
+    email = StringField("E-mail *", validators=[DataRequired(), Email()])
+    contact = IntegerField("Mobile number (optional)", validators=[Optional(), NumberRange(min=1000000000, max=9999999999, message="Contact number should have 10 digits (no spaces)")])
+    password = PasswordField("Password *", validators=[DataRequired()])
+    cnf_password = PasswordField("Confirm Password *", validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField("CREATE MY ACCOUNT")
-    # Check for email already in the database.
+    # check if email in db
     def validate_email(self, email):
         user_mail = User.query.filter_by(email=email.data).first()
         if user_mail:
@@ -21,7 +23,13 @@ class RegistrationForm(FlaskForm):
             raise ValidationError(message)
 
 class LoginForm(FlaskForm):
-    email = StringField("E-mail", validators=[DataRequired(), Email()])
+    email = StringField("E-mail", validators=[DataRequired(), Email()], render_kw={'autofocus': True})
     password = PasswordField("Password", validators=[DataRequired()])
     remember = BooleanField("Remember Me")
     submit = SubmitField("LOGIN")
+    # check if email in db
+    def validate_email(self, email):
+        user_mail = User.query.filter_by(email=email.data).first()
+        if not user_mail: 
+            message = Markup(f"This email is not registered. Please register by <a href='{url_for('sign_up')}'>clicking here</a>.")
+            raise ValidationError(message)
