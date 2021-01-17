b97f270d7354121dcbf3943b9e84827ba0d2d433
Amittras <pal.amittras@gmail.com>
2021-01-17 20:24:13 +0530

    added fullpage.js library for home page. Added relevant scripts and made minor modifications.

---
+++ app/BP_home/assets/js/home.js
@@ -0,0 +1,10 @@
+new fullpage('#fullpage', {
+    autoScrolling: true,
+    navigation: false,
+    // anchors: ['section1', 'section2', 'section3', 'section4', 'section5'],
+    // navigationTooltips: ['Home', 'Recent Projects', 'Services', 'Reviews', 'Contact'],
+    showActiveTooltip: false,
+    scrollingSpeed: 850,
+    controlArrows: false,
+    slidesNavigation: true
+})
