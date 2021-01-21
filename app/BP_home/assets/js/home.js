857831761bdc112d3a171131311fb8f13e0edb86
Amittras <pal.amittras@gmail.com>
2021-01-21 16:53:53 +0530

    other minor changes

--- app/BP_home/assets/js/home.js
+++ app/BP_home/assets/js/home.js
@@ -1,6 +1,6 @@
 // Function to load the selected project - INCOMPLETE
 var projectCards = document.querySelectorAll('#deskShow .card');
 function loadProject(projectID) {
-    document.querySelector('.active-project').classList.remove('active-project');
-    projectCards[projectID].classList.add('active-project');
+    document.querySelector('.bg-dark').classList.remove("bg-dark", "text-light");
+    projectCards[projectID].classList.add("bg-dark", "text-light");
 }
