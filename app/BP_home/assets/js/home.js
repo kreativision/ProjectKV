c9dd24fad4c2dfd50d92a82b47ed4848e9977ce9
Amittras <pal.amittras@gmail.com>
2021-01-22 18:17:47 +0530

    page layout modifications

--- app/BP_home/assets/js/home.js
+++ app/BP_home/assets/js/home.js
@@ -1,6 +1,9 @@
 // Function to load the selected project - INCOMPLETE
 var projectCards = document.querySelectorAll('#deskShow .card');
 function loadProject(projectID) {
-    document.querySelector('.bg-dark').classList.remove("bg-dark", "text-light");
+    // document.querySelector('.bg-dark').classList.remove("bg-dark", "text-light");
+    projectCards.forEach(card => {
+        card.classList.remove("bg-dark", "text-light");
+    });
     projectCards[projectID].classList.add("bg-dark", "text-light");
 }
