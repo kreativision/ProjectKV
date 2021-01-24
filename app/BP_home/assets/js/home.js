d99f7861f8d1a90a79ff8f3575c2fc31531bdc4b
Amittras <pal.amittras@gmail.com>
2021-01-24 20:35:45 +0530

    Home page template updates - added projects & reviews sections

--- app/BP_home/assets/js/home.js
+++ app/BP_home/assets/js/home.js
@@ -1,9 +1,15 @@
 // Function to load the selected project - INCOMPLETE
-var projectCards = document.querySelectorAll('#deskShow .card');
-function loadProject(projectID) {
-    // document.querySelector('.bg-dark').classList.remove("bg-dark", "text-light");
-    projectCards.forEach(card => {
-        card.classList.remove("bg-dark", "text-light");
-    });
-    projectCards[projectID].classList.add("bg-dark", "text-light");
-}
+// var projectCards = document.querySelectorAll('#deskShow .card');
+// projectCards[0].classList.add("bg-dark", "text-light");
+// function loadProject(projectID) {
+//     // document.querySelector('.bg-dark').classList.remove("bg-dark", "text-light");
+//     projectCards.forEach(card => {
+//         card.classList.remove("bg-dark", "text-light");
+//     });
+//     projectCards[projectID].classList.add("bg-dark", "text-light");
+// }
+
+
+// $('#phoneShow .carousel#projectCarousel').carousel({
+//     interval: false,
+// });
--- app/BP_home/home_routes.py
+++ app/BP_home/home_routes.py
@@ -1,6 +1,14 @@
 from flask.templating import render_template
 from app.BP_home import BP_home
 
+
+projects = [
+    ('New Age of Professionals', 'Logo Design', 'We had the honor of designing a brand logo for New Age of Professionals, Bhopal, India.', 'BP_home.static', 'images/bg-about-2.jpg'),
+    ('Fun Food & Fasion', 'Logo Design', 'Designed complete brand package logo design for a fashion and lifestyle magazine', 'BP_home.static', 'images/bg-s1-01.png'),
+    ('Dream Affairs', 'Branding', 'Complete brand package for a wedding phorography studio based out of Bhopal India', 'BP_home.static', 'images/portrait.png'),
+    ('Fun Food & Fashion', 'Poster Design', 'Posters for print and media coverage designed for Fun Food and Fashion Magazine.', 'BP_home.static', 'images/bg-about-2.jpg')
+]
+
 @BP_home.route('/')
 def home():
-    return render_template('home.html', title='Home')
+    return render_template('home.html', title='Home', projects=projects)
