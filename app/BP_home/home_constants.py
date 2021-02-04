fced11185dddc2a77e730ef932f86f3c2905d437
Amittras <pal.amittras@gmail.com>
2021-02-04 13:26:37 +0530

    added constants file for button labels

---
+++ app/BP_home/home_constants.py
@@ -0,0 +1,22 @@
+from markupsafe import Markup
+
+class HomeConstants:
+    labels = {
+        "tncs": "TnC's Apply *",
+        "offerCTA": "PICK YOUR DESIGN NOW",
+        "tagLine": Markup(f"Let's add some Creativity <br> to your Brilliant Vision."),
+        "exploreServicesCTA": "EXPLORE SERVICES",
+        "recentProjectsCTA": "CHECKOUT RECENT PROJECTS",
+        "recentProjectsHead": "Our Projects",
+        "servicesHead": "Popular Services",
+        "allServicesCTA": "BROWSE ALL SERVICES",
+        "allProjectsCTA": "SEE ALL PROJECTS",
+        "reviewHead": "What our Clients Say"
+    }
+
+class ServicesConstants: 
+    labels = {
+        "heading": "Our Services",
+        "getYours": "Get yours @ just",
+        "makeThis": "CUSTOMIZE"
+    }
