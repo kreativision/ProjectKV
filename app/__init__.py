88a0e5d1ae4902081d00fe23bb0695cbf7a3d038
Amittras <pal.amittras@gmail.com>
2021-02-12 14:51:11 +0530

    updated create script to add updated database schema

--- app/__init__.py
+++ app/__init__.py
@@ -14,10 +14,10 @@ from app.BP_account import BP_account
 
 # App config.
 app = Flask(__name__)
-app.config["SECRET_KEY"] = os.environ.get('FLASK_APP_KEY')
+app.config["SECRET_KEY"] = os.environ.get("FLASK_APP_KEY")
 
 # Database config.
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('FLASK_DB_URI')
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("FLASK_DB_URI")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
@@ -51,7 +51,7 @@ app.register_blueprint(BP_account)
 # setting up cli command to bootstrap the database with mock data for development
 import click
 from flask.cli import with_appcontext
-from app.models import User, Catalogue, Service
+from app.models import User, Catalogue, Service, Project, ProjectImage
 
 
 @click.command(name="create")
@@ -65,7 +65,7 @@ def create():
     print("====> Adding Users ")
     # Adding users
     tester = User(
-        username="Amittras Pal", 
+        username="Amittras Pal",
         email="pal.amittras@gmail.com",
         contact="8871822617",
         password=encryptor.generate_password_hash("password").decode("utf-8"),
@@ -76,18 +76,15 @@ def create():
         contact="9887766554",
         password=encryptor.generate_password_hash("admin").decode("utf-8"),
         admin=True,
+        verified=True,
     )
     tester1 = User(
         username="Sukriti Shukla",
         email="sukritishukla306@gmail.com",
         contact="7352545252",
-        password=encryptor.generate_password_hash("password").decode("utf-8")
-        
-
+        password=encryptor.generate_password_hash("password").decode("utf-8"),
     )
-    db.session.add(tester)
-    db.session.add(admin)
-    db.session.add(tester1)
+    db.session.add_all([tester, tester1, admin])
     db.session.commit()
 
     print("====> Adding Services Catalogue ")
@@ -110,8 +107,7 @@ def create():
         description="Visiting cards are more than just contact like title, email, website, address & phone number, it is also a first exposure to the overall image of the business.",
         start_price=249,
     )
-    db.session.add(logos)
-    db.session.add(posters)
+    db.session.add_all([logos, posters])
     db.session.commit()
 
     print("====> Adding Services ")
@@ -121,54 +117,80 @@ def create():
         description="Like a lettermark, a wordmark or logotype is a font-based logo that focuses on a business’ name alone. Like Visa and Coca-Cola",
         price=699,
         catalogue=logos,
+        image="wordgrams.png",
     )
     logo_2 = Service(
         name="Monogram Logo",
         description="Monogramss or lettermarks are logos that consist of letters, usually brand initials. Think about IBM, HBO...Acronyms or a company’s initials",
         price=999,
         catalogue=logos,
+        image="monograms.png",
     )
     logo_3 = Service(
         name="Symbol Logo",
         description="A pictorial mark (sometimes called brand mark or logo symbol) is an icon—or graphic-based logo. For Example, the logo of Apple",
         price=1399,
         catalogue=logos,
+        image="symbols.png",
     )
-    db.session.add(logo_1)
-    db.session.add(logo_2)
-    db.session.add(logo_3)
-    db.session.commit()
     poster_1 = Service(
         name="Web Posters",
         description="Optimized for fast web transfer and good quality viewing on all kinds of screens, these are best for social media campaigns.",
         price=349,
         catalogue=posters,
+        image="web-poster.png",
     )
     poster_2 = Service(
         name="Print Media",
         description="Focused on the picture quality, use our print media designs which are specifically optimized for high quality printing.",
         price=449,
         catalogue=posters,
+        image="print-media.png",
     )
-    db.session.add(poster_1)
-    db.session.add(poster_2)
-    db.session.commit()
     vCard_1 = Service(
         name="One Sided",
         description="A basic card, with the most important contact information on one side with a blank side for last minute additions by hand for extra flair.",
         price=249,
         catalogue=vCards,
+        image="vCardSS.jpg",
     )
     vCard_2 = Service(
         name="Double Sided",
         description="For those who have more to tell, our double sided vCard design makes sure to utilize all the space available for a proper introduction to your clients",
         price=349,
         catalogue=vCards,
+        image="vCardDS.jpg",
     )
-    db.session.add(vCard_1)
-    db.session.add(vCard_2)
+    # addind all services to session and commit
+    db.session.add_all([logo_1, logo_2, logo_3, poster_1, poster_2, vCard_1, vCard_2])
     db.session.commit()
-    print("====> Database created with mock-data ")
+
+    print("====> Adding Projects")
+    project1 = Project(
+        name="Test Project 1",
+        service="Logo Design",
+        description="This is Project 1. Ducimus accusantium dignissimos quod ipsum illo nisi deserunt accusamus nihil, quisquam sunt. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus accusantium dignissimos quod ipsum illo nisi deserunt accusamus nihil, quisquam sunt.",
+    )
+    project2 = Project(
+        name="Project Number 2",
+        service="Branding Package",
+        description="This is Branding Project No. 2. Ducimus accusantium dignissimos quod ipsum illo nisi deserunt accusamus nihil, quisquam sunt. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus accusantium dignissimos quod ipsum illo nisi deserunt accusamus nihil, quisquam sunt.",
+    )
+
+    image1 = ProjectImage(filepath="wordgrams.png", owner_project=project1)
+    image2 = ProjectImage(filepath="monograms.png", owner_project=project1)
+    image3 = ProjectImage(filepath="symbols.png", owner_project=project1)
+    image4 = ProjectImage(filepath="web-poster.png", owner_project=project2)
+    image5 = ProjectImage(filepath="print-media.png", owner_project=project2)
+    image6 = ProjectImage(filepath="vCardSS.jpg", owner_project=project2)
+    image7 = ProjectImage(filepath="vCardDS.jpg", owner_project=project2)
+
+    # adding projects and project images to session and commit
+    db.session.add_all([project1, project2])
+    db.session.add_all([image1, image2, image3, image4, image5, image6, image7])
+    db.session.commit()
+
+    print("====> Database created with mock-data")
 
 
 # Adding command to cli library
