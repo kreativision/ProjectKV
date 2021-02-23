34fde06cb3e72facaf8c7f3238e01e7967f7ef9c
Amittras <pal.amittras@gmail.com>
2021-02-23 20:06:39 +0530

    admin layout update - added templates

---
+++ app/BP_admin/assets/js/admin.js
@@ -0,0 +1,27 @@
+var sidebar = document.querySelector('#sidebar');
+var sidebarToggler = document.querySelector('#sidebarToggle');
+
+/**
+ * Sidebar menu control function.
+ */
+function toggleSidebar(action) {
+    if (action === 'open') {
+        sidebar.classList.add('active');
+    } else {
+        sidebar.classList.remove('active');
+    }
+}
+/**
+ * Method to close the sidebar on clicking outside the sidebar area.
+ */
+document.addEventListener('click', function (e) {
+    if (!sidebar.contains(e.target) && !sidebarToggler.contains(e.target)) {
+        toggleSidebar('close');
+    }
+})
+/**
+ * Toast notification message control - jQuery.
+ */
+$(document).ready(function () {
+    $(".toast").toast('show');
+})
--- app/BP_admin/routes.py
+++ app/BP_admin/routes.py
@@ -4,8 +4,8 @@ from app.decorators import admin_required
 from flask.templating import render_template
 from flask_login import login_required
 
-@BP_admin.route("/admin")
+@BP_admin.route("/admin/home")
 @login_required
 @admin_required
 def home():
-    return render_template("admin.layout.html", title="Admin Home")
+    return render_template("admin-home.html", title="Admin Home")
