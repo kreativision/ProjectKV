debb92013f01c4d9b137b839210e9cb78c403923
Amittras <pal.amittras@gmail.com>
2021-02-26 18:29:03 +0530

    placed fetch APIs in global routes; added API for user details

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,8 @@
 import os
 from flask.helpers import send_from_directory
 from app import app
+from app.models import User
+from flask.json import jsonify
 
 # The App Icon for browsers.
 @app.route("/favicon.ico")
@@ -10,4 +12,21 @@ def favicon():
         os.path.join(app.root_path, "static"),
         "images/favicon.ico",
         mimetype="image/vnd.microsoft.icon",
+    )
+
+
+@app.route("/api/check-email/<string:email>", methods=["GET"])
+def check_email(email):
+    is_user = User.query.filter_by(email=email).first()
+    if is_user:
+        return jsonify({"registered": 1})
+    else:
+        return jsonify({"registered": 0})
+
+
+@app.route("/api/fetch-user/<string:email>", methods=["GET"])
+def fetch_user(email):
+    user = User.query.filter_by(email=email).first()
+    return jsonify(
+        {"contact": user.contact, "username": user.username, "email": user.email}
     )
