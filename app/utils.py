d1522cd2da3088cbebd5c6011c4cdf40b97df3fa
Amittras <pal.amittras@gmail.com>
2021-04-06 09:33:03 +0530

    util function to update the start price of a catalog when services within are updated

--- app/utils.py
+++ app/utils.py
@@ -1,14 +1,14 @@
 # Utility functions to use throughout the app.
-
-from flask_mail import Message
-from flask_login import current_user
-from app import app, mail, db
-from app.models import User, Review
-from threading import Thread
-from app.decorators import threader
-from app import encryptor
 import os
 import secrets
+from flask_mail import Message
+from flask_login import current_user
+from threading import Thread
+from sqlalchemy import func
+
+from app import app, mail, db, encryptor
+from app.models import User, Review, Catalogue, Service
+from app.decorators import threader
 
 # Redacts the email ID to show on the email-sent page.
 def redact_email(mail_id):
@@ -66,16 +66,31 @@ def mark_all_as_reviewed():
         review.status = "REVIEWED"
     db.session.commit()
 
+
 def delete_review(revId):
     review = Review.query.filter_by(id=revId).first()
     db.session.delete(review)
     db.session.commit()
 
+
 def patch_review(review_data):
-    review = Review.query.filter_by(id=review_data['id']).first()
-    review.title = review_data['title'] if review_data.get('title') else review.title
-    review.content = review_data['content'] if review_data.get('content') else review.content
-    review.status = review_data['status']
+    review = Review.query.filter_by(id=review_data["id"]).first()
+    review.title = review_data["title"] if review_data.get("title") else review.title
+    review.content = (
+        review_data["content"] if review_data.get("content") else review.content
+    )
+    review.status = review_data["status"]
     db.session.commit()
     return True
 
+# Function to update the start price of a catalog when services within are updated
+def update_start_price(catalogue_id):
+    catalogue = Catalogue.query.filter(Catalogue.id == catalogue_id).first()
+    new_start_price = (
+        db.session.query(Service.price, func.min(Service.price))
+        .filter(Service.catalogue_id == catalogue_id)
+        .first()
+        ._asdict()["price"]
+    )
+    catalogue.start_price = new_start_price
+    db.session.commit()
